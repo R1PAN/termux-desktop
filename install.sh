77293659bf2a56d19ce79b5ee8f9788bb239e8cb
P∆N <130258244+R1PAN@users.noreply.github.com>
2023-09-19 04:00:22 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+termux-setup-storage
+sleep 1
+clear 
+yes | pkg update
+cd $HOME
+
+pkg upgrade
+pkg install wget -y
+rm $PREFIX/etc/apt/sources.list.d/termux-desktop-xfce.list
+wget -P $PREFIX/etc/apt/sources.list.d https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/gh-pages/termux-desktop-xfce.list
+yes | pkg update
+pkg upgrade
+pkg install x11-repo -y
+pkg install xfce4 tigervnc xfce4-goodies termux-desktop-xfce ttf-microsoft-cascadia pavucontrol-qt firefox -y
+
+rm -rf .vnc
+mkdir .vnc
+echo -e 'xfce4-session &\nxhost + &' >> .vnc/xstartup
+chmod +x .vnc/xstartup
+
+mkdir Desktop 
+mkdir Downloads 
+mkdir Templates 
+mkdir Public 
+mkdir Documents 
+mkdir Pictures 
+mkdir Videos 
+
+
+unzip -o con.zip
+
+rm $PREFIX/bin/s &> /dev/null
+rm $PREFIX/bin/p &> /dev/null
+echo -e 'vncserver\ntermux-open vnc://localhost:1'> $PREFIX/bin/p
+echo -e 'printf "\033[5;31mokeh🙋\\n"\nvncserver -kill :1 &> /dev/null\nvncserver -kill :2 &> /dev/null\nrm ~/.vnc/*.log &> /dev/null\nrm ~/.vnc/*.pid &> /dev/null' >> $PREFIX/bin/s
+chmod +x $PREFIX/bin/s
+chmod +x $PREFIX/bin/p
